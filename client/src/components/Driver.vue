34c739352b483b0bf4e204f910c4cf431eb898b4
NhanHM4 <43784333+NhanHM4@users.noreply.github.com>
2018-12-13 23:29:13 +0700

    App4: Xây dựng giao diện tài xế

--- client/src/components/Driver.vue
+++ client/src/components/Driver.vue
@@ -1,15 +1,46 @@
 <template>
-<div>
-    App 4
+<div class="driver">
+    <div class="container">
+        <div class="row mb-2 mt-2 d-flex justify-content-center">
+            <!-- <div class="col-md-3 col-sm-12  d-flex justify-content-center mb-1">
+                <button class="btn btn-block" >Bắt đầu</button>
+            </div> -->
+            <div class="col-md-3 col-sm-12  d-flex justify-content-center mb-1">
+                <button class="btn btn-primary btn-block" @click="startWaitting" :disabled="isWaitting" v-if="isBusy==null">{{isWaitting?"Đang online....":"Nhận khách"}}</button>
+            </div>
+            <div class="col-md-3 col-sm-12  d-flex justify-content-center  mb-1">
+                <button class="btn btn-danger btn-block" @click="stopWaitting" v-if="isWaitting==true" >Hủy</button>
+                <button class="btn btn-danger btn-block" v-if="isWaitting==false">Thay đổi vị trí</button>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
 export default {
-    name: "Driver"
-}
+    name: "Driver",
+    data() {
+        return {
+            user: {
+                id: 1,
+            },
+            isWaitting: false,
+            isBusy: null,
+        };
+    },
+    methods: {
+      startWaitting(){
+         var self = this;
+         self.isWaitting = true;
+      },
+      stopWaitting(){
+            var self = this;
+            self.isWaitting = !self.isWaitting;
+      }
+    }
+};
 </script>
 
 <style>
-
 </style>
