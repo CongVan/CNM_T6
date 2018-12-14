78aa0985cb93ca3c07eaa147b77f64858c651645
doquyen0796 <dqdocuments@gmail.com>
2018-12-14 09:58:14 +0700

    App 1: Nhận thông tin khách (Viết hàm AddRequest)

--- server/routes/reqest-receiver.js
+++ server/routes/reqest-receiver.js
@@ -1,9 +1,52 @@
 var express = require('express');
+var reqModel = require('../models/request');
 var router = express.Router();
+var appIo = null;
+var config = require('../config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.json({status:'oK'})
 });
 
-module.exports = router;
+// module.exports = router;
+
+router.post('/add-request', (req, res) => {
+    var m = req.body;
+    reqModel.AddRequest(m)
+        .then(results => {
+            var r = results.affectedRows;
+            if (r == 1) {
+                res.json({
+                    result: 1,
+                    msg: "Thành công"
+                });
+                reqModel.GetRequests().then(rows => {
+                    appIo.to(config.roomAdmin).emit('refreshData', rows);
+                });
+                reqModel.GetAllRequests().then(rows => {
+                    appIo.to(config.roomAdmin).emit('refreshAllData', rows);
+                });
+            } else {
+                res.json({
+                    result: -1,
+                    msg: "Thất bại"
+                });
+            }
+
+        })
+        .catch(err => {
+            res.json({
+                result: -1,
+                msg: ""+err
+            });
+        });
+
+
+});
+
+module.exports = function (io) {
+    appIo = io;
+
+    return router;
+}
