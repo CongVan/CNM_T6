6c8ec8d513fd3c06bb8bf426f993f07c6389c105
VanHC <VanHC@VANHC>
2019-01-03 11:31:05 +0700

    add dum sql

---
+++ dumDB/gocar-db_request.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.12, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gocar-db
+-- ------------------------------------------------------
+-- Server version	8.0.12
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `request`
+--
+
+LOCK TABLES `request` WRITE;
+/*!40000 ALTER TABLE `request` DISABLE KEYS */;
+INSERT INTO `request` VALUES (1,'Super man','09090909','nvc q5','Ghi chú gì đó',1,'2018-11-01 00:00:00',NULL,NULL),(2,'Spider man','09090909','111  nvc q5','Ghi chú gì đó',1,'2018-11-01 00:00:00',NULL,NULL),(3,'Iron man','09090909','123  nvc q5','Ghi chú gì đó',1,'2018-11-01 00:00:00',NULL,NULL),(4,'Hug','09090909','1 nvc q5','Ghi chú gì đó',1,'2018-11-01 00:00:00',NULL,NULL),(5,'Doraemon','8989898','bệnh viện từ vũ','đón gấp',1,'2018-11-01 00:00:00',NULL,NULL),(6,'Nobita','123123121','nhà sách nguyễn văn cừ q5','đón lẹ lẹ ',1,'2018-11-01 00:00:00',NULL,NULL),(8,'siu nhơn','09091231','quận 5 hcm','ghi chú nhanh gọn lẹ',1,'2018-12-04 04:31:44',NULL,NULL),(9,'cus12','23112','nguyễn thị thập quận 7 hcm','fafaks falsf làn asdflaksdnf lkasf \n',1,'2018-12-04 05:21:25',NULL,NULL),(10,'luffy','8131314','Tokyo, Japan','a',1,'2018-12-05 04:33:05',NULL,NULL),(11,'123','31','gia dinh q 1','',1,'2018-12-18 07:33:27',NULL,NULL),(12,'a1','12312','thao cam vien ','',1,'2018-12-18 07:37:26',NULL,NULL),(13,'1','1','1','1',1,'2018-12-30 09:23:26',NULL,NULL),(14,'conan','0000000','Quận 10 Hồ chí minh','ghi chú',1,'2018-12-30 09:39:00',NULL,NULL);
+/*!40000 ALTER TABLE `request` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-01-03 11:30:10
